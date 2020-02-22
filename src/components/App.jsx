fb1b8156b65577fed992c34505391e7b687faef0
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-22 10:38:14 -0800

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assests/css/App.css'
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to Week 15 LAB
+    </div>
+  );
+}
+
+export default App;
